8698c4a191455d66c8b710d27d22fe8036faa3d7
Forgeuser <118595281+Forgeuser@users.noreply.github.com>
2022-11-25 11:55:09 +0200

    Add files via upload

---
+++ abstraction.java
@@ -0,0 +1,19 @@
+public class abstraction {
+
+
+    public static void main(String[] args) {
+        //urdad dad = new urdad();
+        urmom mom = new urmom();
+
+
+        mom.idk();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ inhertince.java
@@ -0,0 +1,31 @@
+public class inhertince {
+
+
+    public static void main(String[] args) {
+
+
+        //carr car9 = new carr("tesla");
+        bysicle bys = new bysicle();
+
+        //car9.park(car9);
+
+        bys.park();
+
+        System.out.println(bys.speed);
+
+        System.out.println(bys.wheels);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
