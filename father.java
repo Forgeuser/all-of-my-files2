5c3b513407b62cb9908dd90f60f4cc219eb0982b
Forgeuser <118595281+Forgeuser@users.noreply.github.com>
2022-11-25 19:20:33 +0200

    Add files via upload

---
+++ father.java
@@ -0,0 +1,10 @@
+public class father {
+
+
+    void fatherleavemilk() {
+
+        System.out.println("i am leaving to get milk ");
+
+
+    }
+}
---
+++ mother.java
@@ -0,0 +1,15 @@
+public class mother extends father {
+
+
+    @Override
+
+
+    void fatherleavemilk() {
+
+        System.out.println("same your motherless and fatherless ");
+
+
+    }
+
+
+}
