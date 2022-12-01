38f1e665db034faf4333f0f9dadd8ca9a63189ab
Forgeuser <118595281+Forgeuser@users.noreply.github.com>
2022-12-01 19:29:15 +0200

    Add files via upload

--- balcar.java
+++ balcar.java
@@ -4,9 +4,17 @@ public class balcar {
     private String ballsymancarmodel;
     private int year;
 
+
+
     private boolean trueornowalteh;
 
-    balcar(String ballsymancarmodel,String ballsymancar,int year,boolean trueornowalteh) {
+    private int hehehahas;
+
+    balcar(String ballsymancarmodel,String ballsymancar,int year,boolean trueornowalteh,int hehehahas) {
+
+
+        this.setballsymancar(ballsymancar,ballsymancarmodel,year,trueornowalteh);
+
 
         this.ballsymancar = ballsymancar;
         this.ballsymancarmodel = ballsymancarmodel;
@@ -35,6 +43,35 @@ public class balcar {
         public boolean idk8() {
 
         return trueornowalteh;
+
+
+        }
+
+        public int hehehes() {
+
+
+            return hehehahas;
+        }
+
+
+
+        public void setballsymancar(String ballsymancar,String ballsymancarmodel,int year,boolean trueornowalteh) {
+
+        this.ballsymancar = ballsymancar;
+
+        this.ballsymancarmodel = ballsymancarmodel;
+        this.year = year;
+        this.trueornowalteh = trueornowalteh;
+
+
+
+
+
+
+
+
+
+
 
 
         }
--- encupslation.java
+++ encupslation.java
@@ -3,13 +3,23 @@ public class encupslation {
 
     public static void main(String[] args) {
 
-        balcar c = new balcar("ballsy car","alminium",2022,true);
+        balcar c = new balcar("ballsy car","alminium",2022,true,8);
+        c.setballsymancar("ballsycar2.0","oxygen",2021,false);
+
+
+
+
+
+
 
         System.out.println(c.idk());
         System.out.println(c.idk8());
         System.out.println(c.idk());
         System.out.println(c.idk2());
         System.out.println(c.getMake());
+        System.out.println(c.hehehes());
+
+
 
 
 
