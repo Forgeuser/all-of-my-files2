61d8a91b1c895473ef2bd065999f4be839201b88
Forgeuser <118595281+Forgeuser@users.noreply.github.com>
2022-12-01 18:28:34 +0200

    learned something new and useless at the same ime

---
+++ balcar.java
@@ -0,0 +1,47 @@
+public class balcar {
+
+    private String ballsymancar;
+    private String ballsymancarmodel;
+    private int year;
+
+    private boolean trueornowalteh;
+
+    balcar(String ballsymancarmodel,String ballsymancar,int year,boolean trueornowalteh) {
+
+        this.ballsymancar = ballsymancar;
+        this.ballsymancarmodel = ballsymancarmodel;
+        this.year = year;
+        this.trueornowalteh = trueornowalteh;
+    }
+        public String getMake() {
+
+
+            return ballsymancar;
+        }
+
+        public String idk() {
+
+        return ballsymancarmodel;
+
+
+        }
+
+        public int idk2() {
+        return year;
+
+
+        }
+
+        public boolean idk8() {
+
+        return trueornowalteh;
+
+
+        }
+
+
+
+
+    }
+
+
---
+++ encupslation.java
@@ -0,0 +1,20 @@
+public class encupslation {
+
+
+    public static void main(String[] args) {
+
+        balcar c = new balcar("ballsy car","alminium",2022,true);
+
+        System.out.println(c.idk());
+        System.out.println(c.idk8());
+        System.out.println(c.idk());
+        System.out.println(c.idk2());
+        System.out.println(c.getMake());
+
+
+
+
+
+
+    }
+}
